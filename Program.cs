378f78fb5337ac3646680d8e0ce0670f030d3235
Viktor Gordikov <flaiho@bk.ru>
2023-12-27 12:21:49 +0300

    описан второй метод PrintArrayFinal. Проведена проверка функционала

--- Program.cs
+++ Program.cs
@@ -1,13 +1,22 @@
-﻿string[] arraySourse = new string[4] {"Hello", "2", "world", ":-)"};
+﻿string[] arraySourse = new string[4] {"Hell", "2", "wo", "654"};
 string[] arrayFinal = new string[arraySourse.Length];
 void ArrayFinal(string[] arraySourse, string[] arrayFinal) {
     int item = 0;
-    for (int i = 0; i < arraySourse.length; i++)
+    for (int i = 0; i < arraySourse.Length; i++)
     {
-        if (arraySourse[i].Length <=3)
+        if (arraySourse[i].Length <= 3)
         {
            arrayFinal[item] = arraySourse[i];
-           i++;
+           item++;
         }
     }
 }
+void PrintArrayFinal (string[] array) {
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+ArrayFinal(arraySourse, arrayFinal);
+PrintArrayFinal(arrayFinal);
