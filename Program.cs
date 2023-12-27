2c8ef24ebb36345210ac8a53302d598f8d169f24
Viktor Gordikov <flaiho@bk.ru>
2023-12-27 12:04:01 +0300

    описан первый метод ArrayFinal

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
 ﻿string[] arraySourse = new string[4] {"Hello", "2", "world", ":-)"};
 string[] arrayFinal = new string[arraySourse.Length];
+void ArrayFinal(string[] arraySourse, string[] arrayFinal) {
+    int item = 0;
+    for (int i = 0; i < arraySourse.length; i++)
+    {
+        if (arraySourse[i].Length <=3)
+        {
+           arrayFinal[item] = arraySourse[i];
+           i++;
+        }
+    }
+}
